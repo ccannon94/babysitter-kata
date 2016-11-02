1655f0bc349cafbd47a1e2663583173d86dcc0ab
Chris Cannon <chris.c.cannon@gmail.com>
2016-11-01 20:35:06 -0400

    Add several additional test cases for hours

--- BabysitterKata.Tests/TwentyFourHourTime_Tests.cs
+++ BabysitterKata.Tests/TwentyFourHourTime_Tests.cs
@@ -11,6 +11,10 @@ namespace BabysitterKata.Tests
     public class TwentyFourHourTime_Tests
     {
         [TestCase(18, 13)]
+        [TestCase(20, 57)]
+        [TestCase(4, 15)]
+        [TestCase(15, 0)]
+        [TestCase(23, 32)]
         public void TwentyFourHourTime_HoursInitializeProperly(int hours, int minutes)
         {
             TwentyFourHourTime testTime = new TwentyFourHourTime(hours, minutes);
