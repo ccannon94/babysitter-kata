c738a93c5bdf81c2959ec87e135f0675dc923ba1
Chris Cannon <chris.c.cannon@gmail.com>
2016-11-01 20:23:03 -0400

    Add first test case to TwentyFourHourTime_Test

--- BabysitterKata.Tests/TwentyFourHourTime_Tests.cs
+++ BabysitterKata.Tests/TwentyFourHourTime_Tests.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace BabysitterKata.Tests
 {
     public class TwentyFourHourTime_Tests
     {
+        [TestCase(18, 13)]
+        public void TwentyFourHourTime_HoursInitializeProperly(int hours, int minutes)
+        {
+            TwentyFourHourTime testTime = new TwentyFourHourTime(hours, minutes);
+            Assert.That(testTime.Hours, Is.EqualTo(hours));
+        }
     }
 }
