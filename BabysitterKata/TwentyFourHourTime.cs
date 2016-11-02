71c618c45758cee6ccea0375c30f8fced4d788b0
Chris Cannon <chris.c.cannon@gmail.com>
2016-11-01 20:30:48 -0400

    Add TwentyFourHourTime with constructor

---
+++ BabysitterKata/TwentyFourHourTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabysitterKata
+{
+    class TwentyFourHourTime
+    {
+        private int _hours;
+        private int _minutes;
+
+        public int Hours { get{ return _hours; } }
+        public int Minutes { get { return _minutes; } }
+
+        TwentyFourHourTime(int hours, int minutes)
+        {
+            _hours = hours;
+            _minutes = minutes;
+        }
+    }
+}
