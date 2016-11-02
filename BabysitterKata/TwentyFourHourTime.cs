dc20016d496401535e985dde94f7a9c824fff73d
Chris Cannon <chris.c.cannon@gmail.com>
2016-11-01 21:17:07 -0400

    Eliminate magic numbers from 24 hr time
    Previously, bounds were checked using magic numbers for the min and max
    time for hours and minutes. Now, by using constants, I can easily
    change these in a single location if I decide to change my format, for
    example, to go from 1-24 hours instead of 0-23.

--- BabysitterKata/TwentyFourHourTime.cs
+++ BabysitterKata/TwentyFourHourTime.cs
@@ -8,6 +8,10 @@ namespace BabysitterKata
 {
     public class TwentyFourHourTime
     {
+        private const int MIN_HOURS_VALUE = 0;
+        private const int MIN_MINUTES_VALUE = 0;
+        private const int MAX_HOURS_VALUE = 23;
+        private const int MAX_MINUTES_VALUE = 59;
         private int _hours;
         private int _minutes;
 
@@ -28,14 +32,14 @@ namespace BabysitterKata
 
         private bool ValidateMinuteValue(int minutes)
         {
-            if (minutes < 60 && minutes >= 0)
+            if (minutes <= MAX_MINUTES_VALUE && minutes >= MIN_MINUTES_VALUE)
                 return true;
             return false;
         }
 
         private bool ValidateHourValue(int hours)
         {
-            if (hours < 24 && hours >= 0)
+            if (hours <= MAX_HOURS_VALUE && hours >= MIN_HOURS_VALUE)
                 return true;
             return false;
         }
