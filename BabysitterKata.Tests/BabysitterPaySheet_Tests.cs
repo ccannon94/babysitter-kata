12dae51f2815847301c143f316184f8e635ab461
Chris Cannon <Chris Cannon>
2017-02-09 13:16:33 -0500

    Add test for constructor of BabysitterPaySheet

--- BabysitterKata.Tests/BabysitterPaySheet_Tests.cs
+++ BabysitterKata.Tests/BabysitterPaySheet_Tests.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace BabysitterKata.Tests
 {
     class BabysitterPaySheet_Tests
     {
+        [TestCase(3, 2, 3)]
+        public void BabysitterPaySheet_GivenThreeValidParameters_CreateBabysitterPaySheet(int hrsBeforeBed, int hrsBedToMidnight, int hrsAfterMidnight)
+        {
+            BabysitterPaySheet paySheet = new BabysitterPaySheet(hrsBeforeBed, hrsBedToMidnight, hrsAfterMidnight);
+
+            Assert.IsInstanceOf(typeof(BabysitterPaySheet), paySheet);
+        }
     }
 }
